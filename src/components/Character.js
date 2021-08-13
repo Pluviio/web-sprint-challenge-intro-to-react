311be5754751b2d3b932809c0f9ff1e5ebc3d7d6
courtneyharris <harriscourtney98@gmail.com>
2021-08-13 19:17:36 -0400

    There was more that I wanted to do but for the sake of time this is what I had to do

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,43 @@
-// Write your Character component here
+import React from 'react'
+import styled from 'styled-components'
+
+
+
+const Info = styled.div`
+    color: #E9C46A;
+    background: #264653;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    font-size: 20px;
+    font-weight: bold;
+    margin: 0%;
+    text-align: center;
+    border: 3px solid #E76F51;
+`
+
+const Wrap = styled.div`
+    opacity: 0.9;
+    width: 50%;
+    height: 50%;
+    margin: 0 auto;
+`
+
+function Character(info) {
+    const charInfo = info.props;
+
+    return (
+        <Wrap>
+            {charInfo.map((data, idx) => {
+                return (
+                    <Info key={idx}>
+                        Name: {data.name}<br /> Gender: {data.gender} <br /> Birth Year: {data.birth_year}<br /> <br />Films: {data.films.join(' - ')}
+                    </Info>
+                );
+            })}
+        </Wrap>
+    );
+}
+
+export default Character;
